a59e046a35332aedc430d316a52c8e1da7888908
unknown <hasnatrumman1234@gmail.com>
2018-09-19 16:22:54 +0600

    done CSS

--- index.php
+++ index.php
@@ -10,9 +10,18 @@
 </head>
 <body>
     <div class='wrapper clr'>
+    <header class="headsec clr">
+        <h2>Basic shoutbox with PHP & MySQLi</h2>
+    </header>
         <section class='content clr'>
             <div class='box'>
                 <ul>
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
                    <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
                    <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
                    <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
@@ -28,17 +37,27 @@
                         <tr>
                             <td>Name</td>
                             <td>:</td>
-                            <td><input type="text" name="name" required/></td>
+                            <td><input type="text" name="name" placeholder="Name Please" required/></td>
                         </tr>
                         <tr>
                             <td>Body</td>
                             <td>:</td>
-                            <td><input type="text" name="body" required/></td>
+                            <td><input type="text" name="body" placeholder="Say Something Please"required/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td><input type="submit" value="Shout Loud"/></td>
                         </tr>
                     </table>
                 </form>
             </div>
-        </section>       
+        </section>   
+
+
+        <footer class="footsec clr">
+            <h2>&copy: Copy Rights Reserve</h2>
+        </footer>    
     </div>
 </body>
 </html>
