5965d81b4f111861d93a9f59c30dc8ac9cc83363
unknown <hasnatrumman1234@gmail.com>
2018-09-19 15:31:39 +0600

    done html

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shout Box</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class='wrapper clr'>
+        <section class='content clr'>
+            <div class='box'>
+                <ul>
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                   <li><span>5:30pm</span>-<b>Rumman</b>Hello, howdy</li> 
+                </ul>
+            </div>
+
+            <div class="shoutbox clr">
+                <form>
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>:</td>
+                            <td><input type="text" name="name" required/></td>
+                        </tr>
+                        <tr>
+                            <td>Body</td>
+                            <td>:</td>
+                            <td><input type="text" name="body" required/></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </section>       
+    </div>
+</body>
+</html>
